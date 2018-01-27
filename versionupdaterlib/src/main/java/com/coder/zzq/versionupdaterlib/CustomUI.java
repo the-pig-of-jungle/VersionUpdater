2e132306a98431491cac49686a58d2daf8f4d03e
the-pig-of-jungle <2013070316qiang>
2018-01-27 09:17:30 +0800

    update

--- versionupdaterlib/src/main/java/com/coder/zzq/versionupdaterlib/CustomUI.java
+++ versionupdaterlib/src/main/java/com/coder/zzq/versionupdaterlib/CustomUI.java
@@ -1,9 +1,21 @@
 package com.coder.zzq.versionupdaterlib;
 
 /**
- * Created by 喜欢、陪你看风景 on 2018/1/26.
+ * Created by 朱志强 on 2018/1/26.
  */
 
 public interface CustomUI {
+    void onHasNewVersion();
 
+    void onConfirmDownload();
+
+    void onCancelDownload();
+
+    void onDownloadComplete();
+
+    void onConfirmInstall();
+
+    void onCancelInstall();
+
+    boolean needNotifiedProgress();
 }
---
+++ versionupdaterlib/src/main/java/com/coder/zzq/versionupdaterlib/DefaultUI.java
@@ -0,0 +1,43 @@
+package com.coder.zzq.versionupdaterlib;
+
+/**
+ * Created by 喜欢、陪你看风景 on 2018/1/26.
+ */
+
+public class DefaultUI implements CustomUI {
+    
+    @Override
+    public void onHasNewVersion() {
+
+    }
+
+    @Override
+    public void onConfirmDownload() {
+
+    }
+
+    @Override
+    public void onCancelDownload() {
+
+    }
+
+    @Override
+    public void onDownloadComplete() {
+
+    }
+
+    @Override
+    public void onConfirmInstall() {
+
+    }
+
+    @Override
+    public void onCancelInstall() {
+
+    }
+
+    @Override
+    public boolean needNotifiedProgress() {
+        return false;
+    }
+}
